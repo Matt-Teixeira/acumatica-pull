42b1beb43123b6720a6b5e543fa7038d84df92a5
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-01-19 23:03:46 +0000

    Init redis implementation

---
+++ server/redis/index.js
@@ -0,0 +1,41 @@
+("use strict");
+require("dotenv").config();
+const { log } = require("../logger");
+const redis = require("redis");
+
+async function initRedis() {
+  try {
+    console.log(process.env.REDIS_PORT);
+    // SETUP ENV BASED RESOURCES -> REDIS CLIENT, JOB SCHEDULES
+    const clienConfig = {
+      socket: {
+        port: process.env.REDIS_PORT,
+        host: process.env.REDIS_IP,
+      },
+    };
+
+    const redisClient = redis.createClient(clienConfig);
+
+    redisClient.on(
+      "error",
+      async (error) =>
+        await log("error", "NA", "NA", "initRedis", `ON ERROR`, {
+          // TODO: KILL APP?
+          error: error,
+        })
+    );
+
+    await redisClient.connect();
+
+    return redisClient;
+  } catch (error) {
+    await redisClient.quit();
+    await log("error", "NA", "NA", "initRedis", `FN CALL`, {
+      error: error,
+    });
+  }
+}
+
+initRedis();
+
+module.exports = initRedis;
